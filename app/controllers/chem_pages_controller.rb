82e4ce0ac7130472deacbf899a93cf248336eb0a
David Morton <55235981+davidmorton0@users.noreply.github.com>
2019-10-31 11:58:40 +0000

    Add main pages controller

---
+++ app/controllers/chem_pages_controller.rb
@@ -0,0 +1,7 @@
+class ChemPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'chem_pages/home'
+  get 'chem_pages/help'
   root 'application#hello'
 end
