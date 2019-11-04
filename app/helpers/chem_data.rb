4ef2fb41534ec51a8fcfa74afd41d0fc56b6e1c9
David Morton <55235981+davidmorton0@users.noreply.github.com>
2019-11-04 15:12:11 +0000

    Added quiz with random questions

---
+++ app/helpers/chem_data.rb
@@ -0,0 +1,984 @@
+class ChemData
+  Element = [
+    {
+      Ar: 1,
+      Name: 'Hydrogen',
+      Symbol: 'H',
+      Group: 1,
+      Period: 1,
+      Block: 's',
+      State: 'Gas',
+      Description: 'Non-metal'
+    },
+    {
+      Ar: 2,
+      Name: 'Helium',
+      Symbol: 'He',
+      Group: 18,
+      Period: 1,
+      Block: 's',
+      State: 'Gas',
+      Description: 'Noble gas'
+    },
+    {
+      Ar: 3,
+      Name: 'Lithium',
+      Symbol: 'Li',
+      Group: 1,
+      Period: 2,
+      Block: 's',
+      State: 'Solid',
+      Description: 'Alkali metal'
+    },
+    {
+      Ar: 4,
+      Name: 'Beryllium',
+      Symbol: 'Be',
+      Group: 2,
+      Period: 2,
+      Block: 's',
+      State: 'Solid',
+      Description: 'Alkaline earth metal'
+    },
+    {
+      Ar: 5,
+      Name: 'Boron',
+      Symbol: 'B',
+      Group: 13,
+      Period: 2,
+      Block: 'p',
+      State: 'Solid',
+      Description: 'Metalloid'
+    },
+    {
+      Ar: 6,
+      Name: 'Carbon',
+      Symbol: 'C',
+      Group: 14,
+      Period: 2,
+      Block: 'p',
+      State: 'Solid',
+      Description: 'Non-metal'
+    },
+    {
+      Ar: 7,
+      Name: 'Nitrogen',
+      Symbol: 'N',
+      Group: 15,
+      Period: 2,
+      Block: 'p',
+      State: 'Gas',
+      Description: 'Non-metal'
+    },
+    {
+      Ar: 8,
+      Name: 'Oxygen',
+      Symbol: 'O',
+      Group: 16,
+      Period: 2,
+      Block: 'p',
+      State: 'Gas',
+      Description: 'Non-metal'
+    },
+    {
+      Ar: 9,
+      Name: 'Fluorine',
+      Symbol: 'F',
+      Group: 17,
+      Period: 2,
+      Block: 'p',
+      State: 'Gas',
+      Description: 'Halogen'
+    },
+    {
+      Ar: 10,
+      Name: 'Neon',
+      Symbol: 'Ne',
+      Group: 18,
+      Period: 2,
+      Block: 'p',
+      State: 'Gas',
+      Description: 'Noble gas'
+    },
+    {
+      Ar: 11,
+      Name: 'Sodium',
+      Symbol: 'Na',
+      Group: 1,
+      Period: 3,
+      Block: 's',
+      State: 'Solid',
+      Description: 'Alkali metal'
+    },
+    {
+      Ar: 12,
+      Name: 'Magnesium',
+      Symbol: 'Mg',
+      Group: 2,
+      Period: 3,
+      Block: 's',
+      State: 'Solid',
+      Description: 'Alkaline earth metal'
+    },
+    {
+      Ar: 13,
+      Name: 'Aluminium',
+      Symbol: 'Al',
+      Group: 13,
+      Period: 3,
+      Block: 'p',
+      State: 'Solid',
+      Description: 'Metal'
+    },
+    {
+      Ar: 14,
+      Name: 'Silicon',
+      Symbol: 'Si',
+      Group: 14,
+      Period: 3,
+      Block: 'p',
+      State: 'Solid',
+      Description: 'Metalloid'
+    },
+    {
+      Ar: 15,
+      Name: 'Phosphorus',
+      Symbol: 'P',
+      Group: 15,
+      Period: 3,
+      Block: 'p',
+      State: 'Solid',
+      Description: 'Non-metal'
+    },
+    {
+      Ar: 16,
+      Name: 'Sulfur',
+      Symbol: 'S',
+      Group: 16,
+      Period: 3,
+      Block: 'p',
+      State: 'Solid',
+      Description: 'Non-metal'
+    },
+    {
+      Ar: 17,
+      Name: 'Chlorine',
+      Symbol: 'Cl',
+      Group: 17,
+      Period: 3,
+      Block: 'p',
+      State: 'Gas',
+      Description: 'Halogen'
+    },
+    {
+      Ar: 18,
+      Name: 'Argon',
+      Symbol: 'Ar',
+      Group: 18,
+      Period: 3,
+      Block: 'p',
+      State: 'Gas',
+      Description: 'Noble gas'
+    },
+    {
+      Ar: 19,
+      Name: 'Potassium',
+      Symbol: 'K',
+      Group: 1,
+      Period: 4,
+      Block: 's',
+      State: 'Solid',
+      Description: 'Alkali metal'
+    },
+    {
+      Ar: 20,
+      Name: 'Calcium',
+      Symbol: 'Ca',
+      Group: 2,
+      Period: 4,
+      Block: 's',
+      State: 'Solid',
+      Description: 'Alkaline earth metal'
+    },
+    {
+      Ar: 21,
+      Name: 'Scandium',
+      Symbol: 'Sc',
+      Group: 3,
+      Period: 4,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 22,
+      Name: 'Titanium',
+      Symbol: 'Ti',
+      Group: 4,
+      Period: 4,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 23,
+      Name: 'Vanadium',
+      Symbol: 'V',
+      Group: 5,
+      Period: 4,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 24,
+      Name: 'Chromium',
+      Symbol: 'Cr',
+      Group: 6,
+      Period: 4,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 25,
+      Name: 'Manganese',
+      Symbol: 'Mn',
+      Group: 7,
+      Period: 4,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 26,
+      Name: 'Iron',
+      Symbol: 'Fe',
+      Group: 8,
+      Period: 4,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 27,
+      Name: 'Cobalt',
+      Symbol: 'Co',
+      Group: 9,
+      Period: 4,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 28,
+      Name: 'Nickel',
+      Symbol: 'Ni',
+      Group: 10,
+      Period: 4,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 29,
+      Name: 'Copper',
+      Symbol: 'Cu',
+      Group: 11,
+      Period: 4,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 30,
+      Name: 'Zinc',
+      Symbol: 'Zn',
+      Group: 12,
+      Period: 4,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 31,
+      Name: 'Gallium',
+      Symbol: 'Ga',
+      Group: 13,
+      Period: 4,
+      Block: 'p',
+      State: 'Solid',
+      Description: 'Metal'
+    },
+    {
+      Ar: 32,
+      Name: 'Germanium',
+      Symbol: 'Ge',
+      Group: 14,
+      Period: 4,
+      Block: 'p',
+      State: 'Solid',
+      Description: 'Metalloid'
+    },
+    {
+      Ar: 33,
+      Name: 'Arsenic',
+      Symbol: 'As',
+      Group: 15,
+      Period: 4,
+      Block: 'p',
+      State: 'Solid',
+      Description: 'Metalloid'
+    },
+    {
+      Ar: 34,
+      Name: 'Selenium',
+      Symbol: 'Se',
+      Group: 16,
+      Period: 4,
+      Block: 'p',
+      State: 'Solid',
+      Description: 'Non-metal'
+    },
+    {
+      Ar: 35,
+      Name: 'Bromine',
+      Symbol: 'Br',
+      Group: 17,
+      Period: 4,
+      Block: 'p',
+      State: 'Liquid',
+      Description: 'Halogen'
+    },
+    {
+      Ar: 36,
+      Name: 'Krypton',
+      Symbol: 'Kr',
+      Group: 18,
+      Period: 4,
+      Block: 'p',
+      State: 'Gas',
+      Description: 'Noble gas'
+    },
+    {
+      Ar: 37,
+      Name: 'Rubidium',
+      Symbol: 'Rb',
+      Group: 1,
+      Period: 5,
+      Block: 's',
+      State: 'Solid',
+      Description: 'Alkali metal'
+    },
+    {
+      Ar: 38,
+      Name: 'Strontium',
+      Symbol: 'Sr',
+      Group: 2,
+      Period: 5,
+      Block: 's',
+      State: 'Solid',
+      Description: 'Alkaline earth metal'
+    },
+    {
+      Ar: 39,
+      Name: 'Yttrium',
+      Symbol: 'Y',
+      Group: 3,
+      Period: 5,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 40,
+      Name: 'Zirconium',
+      Symbol: 'Zr',
+      Group: 4,
+      Period: 5,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 41,
+      Name: 'Niobium',
+      Symbol: 'Nb',
+      Group: 5,
+      Period: 5,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 42,
+      Name: 'Molybdenum',
+      Symbol: 'Mo',
+      Group: 6,
+      Period: 5,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 43,
+      Name: 'Technetium',
+      Symbol: 'Tc',
+      Group: 7,
+      Period: 5,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 44,
+      Name: 'Ruthenium',
+      Symbol: 'Ru',
+      Group: 8,
+      Period: 5,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 45,
+      Name: 'Rhodium',
+      Symbol: 'Rh',
+      Group: 9,
+      Period: 5,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 46,
+      Name: 'Palladium',
+      Symbol: 'Pd',
+      Group: 10,
+      Period: 5,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 47,
+      Name: 'Silver',
+      Symbol: 'Ag',
+      Group: 11,
+      Period: 5,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 48,
+      Name: 'Cadmium',
+      Symbol: 'Cd',
+      Group: 12,
+      Period: 5,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 49,
+      Name: 'Indium',
+      Symbol: 'In',
+      Group: 13,
+      Period: 5,
+      Block: 'p',
+      State: 'Solid',
+      Description: 'Metal'
+    },
+    {
+      Ar: 50,
+      Name: 'Tin',
+      Symbol: 'Sn',
+      Group: 14,
+      Period: 5,
+      Block: 'p',
+      State: 'Solid',
+      Description: 'Metal'
+    },
+    {
+      Ar: 51,
+      Name: 'Antimony',
+      Symbol: 'Sb',
+      Group: 15,
+      Period: 5,
+      Block: 'p',
+      State: 'Solid',
+      Description: 'Metalloid'
+    },
+    {
+      Ar: 52,
+      Name: 'Tellurium',
+      Symbol: 'Te',
+      Group: 16,
+      Period: 5,
+      Block: 'p',
+      State: 'Solid',
+      Description: 'Metalloid'
+    },
+    {
+      Ar: 53,
+      Name: 'Iodine',
+      Symbol: 'I',
+      Group: 17,
+      Period: 5,
+      Block: 'p',
+      State: 'Solid',
+      Description: 'Halogen'
+    },
+    {
+      Ar: 54,
+      Name: 'Xenon',
+      Symbol: 'Xe',
+      Group: 18,
+      Period: 5,
+      Block: 'p',
+      State: 'Gas',
+      Description: 'Noble gas'
+    },
+    {
+      Ar: 55,
+      Name: 'Caesium',
+      Symbol: 'Cs',
+      Group: 1,
+      Period: 6,
+      Block: 's',
+      State: 'Solid',
+      Description: 'Alkali metal'
+    },
+    {
+      Ar: 56,
+      Name: 'Barium',
+      Symbol: 'Ba',
+      Group: 2,
+      Period: 6,
+      Block: 's',
+      State: 'Solid',
+      Description: 'Alkaline earth metal'
+    },
+    {
+      Ar: 57,
+      Name: 'Lanthanum',
+      Symbol: 'La',
+      Group: 3,
+      Period: 6,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Lanthanide'
+    },
+    {
+      Ar: 58,
+      Name: 'Cerium',
+      Symbol: 'Ce',
+      Group: 3,
+      Period: 6,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Lanthanide'
+    },
+    {
+      Ar: 59,
+      Name: 'Praseodymium',
+      Symbol: 'Pr',
+      Group: 3,
+      Period: 6,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Lanthanide'
+    },
+    {
+      Ar: 60,
+      Name: 'Neodymium',
+      Symbol: 'Nd',
+      Group: 3,
+      Period: 6,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Lanthanide'
+    },
+    {
+      Ar: 61,
+      Name: 'Promethium',
+      Symbol: 'Pm',
+      Group: 3,
+      Period: 6,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Lanthanide'
+    },
+    {
+      Ar: 62,
+      Name: 'Samarium',
+      Symbol: 'Sm',
+      Group: 3,
+      Period: 6,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Lanthanide'
+    },
+    {
+      Ar: 63,
+      Name: 'Europium',
+      Symbol: 'Eu',
+      Group: 3,
+      Period: 6,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Lanthanide'
+    },
+    {
+      Ar: 64,
+      Name: 'Gadolinium',
+      Symbol: 'Gd',
+      Group: 3,
+      Period: 6,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Lanthanide'
+    },
+    {
+      Ar: 65,
+      Name: 'Terbium',
+      Symbol: 'Tb',
+      Group: 3,
+      Period: 6,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Lanthanide'
+    },
+    {
+      Ar: 66,
+      Name: 'Dysprosium',
+      Symbol: 'Dy',
+      Group: 3,
+      Period: 6,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Lanthanide'
+    },
+    {
+      Ar: 67,
+      Name: 'Holmium',
+      Symbol: 'Ho',
+      Group: 3,
+      Period: 6,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Lanthanide'
+    },
+    {
+      Ar: 68,
+      Name: 'Erbium',
+      Symbol: 'Er',
+      Group: 3,
+      Period: 6,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Lanthanide'
+    },
+    {
+      Ar: 69,
+      Name: 'Thulium',
+      Symbol: 'Tm',
+      Group: 3,
+      Period: 6,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Lanthanide'
+    },
+    {
+      Ar: 70,
+      Name: 'Ytterbium',
+      Symbol: 'Yb',
+      Group: 3,
+      Period: 6,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Lanthanide'
+    },
+    {
+      Ar: 71,
+      Name: 'Lutetium',
+      Symbol: 'Lu',
+      Group: 3,
+      Period: 6,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Lanthanide'
+    },
+    {
+      Ar: 72,
+      Name: 'Hafnium',
+      Symbol: 'Hf',
+      Group: 4,
+      Period: 6,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 73,
+      Name: 'Tantalum',
+      Symbol: 'Ta',
+      Group: 5,
+      Period: 6,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 74,
+      Name: 'Tungsten',
+      Symbol: 'W',
+      Group: 6,
+      Period: 6,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 75,
+      Name: 'Rhenium',
+      Symbol: 'Re',
+      Group: 7,
+      Period: 6,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 76,
+      Name: 'Osmium',
+      Symbol: 'Os',
+      Group: 8,
+      Period: 6,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 77,
+      Name: 'Iridium',
+      Symbol: 'Ir',
+      Group: 9,
+      Period: 6,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 78,
+      Name: 'Platinum',
+      Symbol: 'Pt',
+      Group: 10,
+      Period: 6,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 79,
+      Name: 'Gold',
+      Symbol: 'Au',
+      Group: 11,
+      Period: 6,
+      Block: 'd',
+      State: 'Solid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 80,
+      Name: 'Mercury',
+      Symbol: 'Hg',
+      Group: 12,
+      Period: 6,
+      Block: 'd',
+      State: 'Liquid',
+      Description: 'Transition metal'
+    },
+    {
+      Ar: 81,
+      Name: 'Thallium',
+      Symbol: 'Tl',
+      Group: 13,
+      Period: 6,
+      Block: 'p',
+      State: 'Solid',
+      Description: 'Metal'
+    },
+    {
+      Ar: 82,
+      Name: 'Lead',
+      Symbol: 'Pb',
+      Group: 14,
+      Period: 6,
+      Block: 'p',
+      State: 'Solid',
+      Description: 'Metal'
+    },
+    {
+      Ar: 83,
+      Name: 'Bismuth',
+      Symbol: 'Bi',
+      Group: 15,
+      Period: 6,
+      Block: 'p',
+      State: 'Solid',
+      Description: 'Metal'
+    },
+    {
+      Ar: 84,
+      Name: 'Polonium',
+      Symbol: 'Po',
+      Group: 16,
+      Period: 6,
+      Block: 'p',
+      State: 'Solid',
+      Description: 'Metal'
+    },
+    {
+      Ar: 85,
+      Name: 'Astatine',
+      Symbol: 'At',
+      Group: 17,
+      Period: 6,
+      Block: 'p',
+      State: 'Solid',
+      Description: 'Halogen'
+    },
+    {
+      Ar: 86,
+      Name: 'Radon',
+      Symbol: 'Rn',
+      Group: 18,
+      Period: 6,
+      Block: 'p',
+      State: 'Gas',
+      Description: 'Noble gas'
+    },
+    {
+      Ar: 87,
+      Name: 'Francium',
+      Symbol: 'Fr',
+      Group: 1,
+      Period: 7,
+      Block: 's',
+      State: 'Solid',
+      Description: 'Alkali metal'
+    },
+    {
+      Ar: 88,
+      Name: 'Radium',
+      Symbol: 'Ra',
+      Group: 2,
+      Period: 7,
+      Block: 's',
+      State: 'Solid',
+      Description: 'Alkaline earth metal'
+    },
+    {
+      Ar: 89,
+      Name: 'Actinium',
+      Symbol: 'Ac',
+      Group: 3,
+      Period: 7,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Actinide'
+    },
+    {
+      Ar: 90,
+      Name: 'Thorium',
+      Symbol: 'Th',
+      Group: 3,
+      Period: 7,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Actinide'
+    },
+    {
+      Ar: 91,
+      Name: 'Protactinium',
+      Symbol: 'Pa',
+      Group: 3,
+      Period: 7,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Actinide'
+    },
+    {
+      Ar: 92,
+      Name: 'Uranium',
+      Symbol: 'U',
+      Group: 3,
+      Period: 7,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Actinide'
+    },
+    {
+      Ar: 93,
+      Name: 'Neptunium',
+      Symbol: 'Np',
+      Group: 3,
+      Period: 7,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Actinide'
+    },
+    {
+      Ar: 94,
+      Name: 'Plutonium',
+      Symbol: 'Pu',
+      Group: 3,
+      Period: 7,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Actinide'
+    },
+    {
+      Ar: 95,
+      Name: 'Americium',
+      Symbol: 'Am',
+      Group: 3,
+      Period: 7,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Actinide'
+    },
+    {
+      Ar: 96,
+      Name: 'Curium',
+      Symbol: 'Cm',
+      Group: 3,
+      Period: 7,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Actinide'
+    },
+    {
+      Ar: 97,
+      Name: 'Berkelium',
+      Symbol: 'Bk',
+      Group: 3,
+      Period: 7,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Actinide'
+    },
+    {
+      Ar: 98,
+      Name: 'Californium',
+      Symbol: 'Cf',
+      Group: 3,
+      Period: 7,
+      Block: 'f',
+      State: 'Solid',
+      Description: 'Actinide'
+    },
+  ]
+end
