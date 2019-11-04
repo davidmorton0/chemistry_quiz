e74e4ff12578a42577e76e59a4445c5137678fc0
David Morton <david@example.com>
2019-11-04 15:12:11 +0000

    Added quiz with random questions

--- app/controllers/chem_pages_controller.rb
+++ app/controllers/chem_pages_controller.rb
@@ -19,13 +19,14 @@ class ChemPagesController < ApplicationController
   end
 
   def do_quiz
+    @num_questions = 10
     @question_number = params[:quiz][:question_number].to_i
     @score = params[:quiz][:score].to_i
     if params[:quiz][:page] == 'question'
       @page = :answer
       render 'chem_pages/quiz'    
     elsif params[:quiz][:page] == 'answer'
-      if @question_number == 4
+      if @question_number == @num_questions
         render 'chem_pages/result'
       else
         @page = :question
