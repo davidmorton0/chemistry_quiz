51eda5a5c3193305c12fdcb73f66fccc810ccddd
David Morton <55235981+davidmorton0@users.noreply.github.com>
2019-11-13 22:57:50 +0000

    database fixing

--- db/migrate/20191113214618_remove_answer_a_from_questions.rb
+++ db/migrate/20191113214618_remove_answer_a_from_questions.rb
@@ -3,15 +3,9 @@ class RemoveAnswerAFromQuestions < ActiveRecord::Migration[6.0]
 
     remove_column :questions, :answer_A, :string
 
-    remove_column :questions, :remove_answer_B_from_questions, :string
-
     remove_column :questions, :answer_B, :string
 
-    remove_column :questions, :remove_answer_C_from_questions, :string
-
     remove_column :questions, :answer_C, :string
-
-    remove_column :questions, :remove_answer_D_from_questions, :string
 
     remove_column :questions, :answer_D, :string
   end
