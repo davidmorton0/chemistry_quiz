61c7a003c6ab77e32d92f4b50168303202ef950a
David Morton <55235981+davidmorton0@users.noreply.github.com>
2019-11-21 11:14:00 +0000

    admin links

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,58 +7,77 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     @non_admin = users(:archer)
   end
   
-  test "layout links when logged in as admin" do
+  test "header links when logged in as admin" do
     @user = @admin
     log_in_as(@user)
     get root_path
     assert_template 'chem_pages/home'
-    assert_select "a[href=?]", root_path, count: 4
-    assert_select "a[href=?]", user_path(@user), count: 1
-    assert_select "a[href=?]", users_path, count: 1
-    assert_select "a[href=?]", help_path, count: 1
-    assert_select "a[href=?]", about_path, count: 1
-    assert_select "a[href=?]", contact_path, count: 1
-    assert_select "a[href=?]", signup_path, count: 1
-    assert_select "a[href=?]", quiz_types_path, count: 1
-    assert_select "a[href=?]", quiz_path, count: 1
-    assert_select "a[href=?]", logout_path, count: 1
-    assert_select "a[href=?]", edit_user_path(@user), count: 1
-    assert_select "a[href=?]", "#", count: 1
-    assert_select "a", count: 15
+    assert_template partial: '_header', count: 1
+    assert_select "header" do
+      assert_select "a[href=?]", root_path, count: 2
+      assert_select "a[href=?]", help_path, count: 1
+      assert_select "a[href=?]", user_path(@user), count: 1
+      assert_select "a[href=?]", "#", count: 2
+      assert_select "a[href=?]", users_path, count: 1
+      assert_select "a[href=?]", questions_path, count: 1
+      assert_select "a[href=?]", edit_user_path(@user), count: 1
+      assert_select "a[href=?]", logout_path, count: 1
+      assert_select "a", count: 10
+    end
   end
   
-  test "layout links when logged in as non-admin" do
+  test "header links when logged in as non-admin" do
     @user = @non_admin
     log_in_as(@user)
     get root_path
     assert_template 'chem_pages/home'
-    assert_select "a[href=?]", root_path, count: 4
-    assert_select "a[href=?]", user_path(@user), count: 1
-    assert_select "a[href=?]", help_path, count: 1
-    assert_select "a[href=?]", about_path, count: 1
-    assert_select "a[href=?]", contact_path, count: 1
-    assert_select "a[href=?]", signup_path, count: 1
-    assert_select "a[href=?]", quiz_types_path, count: 1
-    assert_select "a[href=?]", quiz_path, count: 1
-    assert_select "a[href=?]", logout_path, count: 1
-    assert_select "a[href=?]", edit_user_path(@user), count: 1
-    assert_select "a[href=?]", "#", count: 1
-    assert_select "a", count: 14
+    assert_template partial: '_header', count: 1
+    assert_select "header" do
+      assert_select "a[href=?]", root_path, count: 2
+      assert_select "a[href=?]", help_path, count: 1
+      assert_select "a[href=?]", user_path(@user), count: 1
+      assert_select "a[href=?]", "#", count: 1
+      assert_select "a[href=?]", edit_user_path(@user), count: 1
+      assert_select "a[href=?]", logout_path, count: 1
+      assert_select "a", count: 7
+    end
   end
   
-  test "layout links when logged out" do
+  test "header links when logged out" do
     delete logout_path
     get root_path
     assert_template 'chem_pages/home'
-    assert_select "a[href=?]", root_path, count: 4
-    assert_select "a[href=?]", help_path, count: 1
-    assert_select "a[href=?]", login_path, count: 1
-    assert_select "a[href=?]", about_path, count: 1
-    assert_select "a[href=?]", contact_path, count: 1
-    assert_select "a[href=?]", signup_path, count: 1
-    assert_select "a[href=?]", quiz_types_path, count: 1
-    assert_select "a[href=?]", quiz_path, count: 1
-    assert_select "a", count: 11
+    assert_template partial: '_header', count: 1
+    assert_select "header" do
+      assert_select "a[href=?]", root_path, count: 2
+      assert_select "a[href=?]", help_path, count: 1
+      assert_select "a[href=?]", login_path, count: 1
+      assert_select "a", count: 4
+    end
+  end
+
+  test "footer links" do
+    get root_path
+    assert_template 'chem_pages/home'
+    assert_template partial: '_footer', count: 1
+    assert_select "footer" do
+      assert_select "a[href=?]", root_path, count: 1
+      assert_select "a[href=?]", about_path, count: 1
+      assert_select "a[href=?]", contact_path, count: 1
+      assert_select "a", count: 3
+    end
   end
   
+  test "main page links" do
+    get root_path
+    assert_template 'chem_pages/home'
+    assert_template partial: '_head', count: 1
+    assert_template partial: '_shim', count: 1
+    assert_select "div.center" do
+      assert_select "a[href=?]", signup_path, count: 1
+      assert_select "a[href=?]", quiz_types_path, count: 1
+      assert_select "a[href=?]", quiz_path, count: 1
+      assert_select "a", count: 3
+    end
+  end
 end
