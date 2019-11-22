9548c7fcae21eb8b8d853c939df2008b721907cd
David Morton <davidmorton0@gmail.com>
2019-11-22 22:14:44 +0000

    home image

--- app/helpers/quizzes_helper.rb
+++ app/helpers/quizzes_helper.rb
@@ -1,2 +1,9 @@
 module QuizzesHelper
+  def check(question, answer)
+    check = answer.text == question.chosen_answer ? answer.text == question.correct_answer ? "\u{2714}" : "\u{274C}" : ""
+  end
+  
+  def show_answered(question, answer)
+    question.answered && (question.correct_answer == answer.text) ? "correct" : "incorrect"
+  end
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :question
+  has_one :quiz, through: :question
   validates :text,  presence: true
   validates :question,  presence: true
 end
