a6a369706423c8e6a21f4b08131276cadbc0b3de
David Morton <davidmorton0@gmail.com>
2019-11-28 15:05:32 +0000

    flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: [:index, :destroy]
 
   def index
-    @users = User.where(activated: true).paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page], per_page: 10)
   end
 
   def show
