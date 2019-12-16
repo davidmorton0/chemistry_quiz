4c6715968d25764efe501f894f0da561d8c18bb2
David Morton <55235981+davidmorton0@users.noreply.github.com>
2019-12-16 13:48:40 +0000

    finished helper tests

--- test/helpers/users_helper_test.rb
+++ test/helpers/users_helper_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersHelperTest < ActionView::TestCase
 
   test "should format time correctly" do
+    assert_equal format_time(nil), "-"
     assert_equal format_time(45), "45s"
     assert_equal format_time(100), "1m 40s"
     assert_equal format_time(4000), "1h 6m 40s"
